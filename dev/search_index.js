f93fdd6aead639f996382617d95f5f59ce1e53dc
Documenter.jl <documenter@juliadocs.github.io>
2021-04-02 19:58:51 +0000

    build based on 598197b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpgpGpu","category":"page"},{"location":"#SpgpGpu","page":"Home","title":"SpgpGpu","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpgpGpu.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpgpGpu]","category":"page"}]
+}
